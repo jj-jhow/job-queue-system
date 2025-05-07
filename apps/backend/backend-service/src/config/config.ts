3759ceb7878cd421558b3178e9e436deff2e98a0
Jonathan Madeira <jonathan.madeira94@gmail.com>
2025-05-07 18:42:53 -0500

    feat: update JOB_QUEUE_NAME to use environment variable for flexibility

--- apps/backend/backend-service/src/config/config.ts
+++ apps/backend/backend-service/src/config/config.ts
@@ -2,7 +2,7 @@
 export const PORT = process.env.PORT || 3000;
 export const REDIS_HOST = process.env.REDIS_HOST || 'localhost';
 export const REDIS_PORT = parseInt(process.env.REDIS_PORT || '6379', 10);
-export const JOB_QUEUE_NAME = 'jobQueueBullMQ';
+export const JOB_QUEUE_NAME = process.env.JOB_QUEUE_NAME || 'local-job-queue';
 
 // Redis connection config for BullMQ
 export const redisConnection = {
